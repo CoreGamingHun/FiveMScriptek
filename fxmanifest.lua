6e37b90d92b580584433296256ffb00c1ffec952
CoreGamingHun <debellamartin7@gmail.com>
2025-06-14 17:29:00 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,15 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'ChatGPT'
-description 'Pizza futár meló ESX-hez'
+author 'DebellaMartin'
+description 'ESX Pizza futár meló menüs járműválasztással és pizzéria belső animációval'
 version '1.0.0'
 
 client_scripts {
-    'client.lua'
+    'client.lua',
 }
 
 server_scripts {
-    '@mysql-async/lib/MySQL.lua', -- ha van mysql és adatbázis, vagy törölhető
-    'server.lua'
+    '@mysql-async/lib/MySQL.lua', -- ha nincs, töröld ki
+    'server.lua',
 }
